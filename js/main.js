9d2042a8fb9d19c6a2f86f165da5fb6ba6bed956
gleb-zavizenov <g_zavizenov@fanshaweonline.ca>
2019-07-25 14:37:40 -0400

    Changed layout and added reset functionality

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 (() => {
 
-	const instruments = document.querySelectorAll(".instrument-icon");
-	const instrumentSets = document.querySelectorAll(".instrument-set");
-    const dropZones = document.querySelectorAll(".dropzone");
-    const playbtn = document.querySelector("#play-btn");
-    const controller = document.querySelector('.controller');
-    const questionBtn = document.querySelector(".question-button");
-	const closeBtn = document.querySelector(".close-btn");
-	const explainerBox = document.querySelector(".explainer-box");
-	const resetBtn = document.querySelector('.reset-button');
+	const instruments = document.querySelectorAll(".instrument-icon"),
+    dropZones = document.querySelectorAll(".dropzone"),
+    playbtn = document.querySelector("#play-btn"),
+    controller = document.querySelector('.controller'),
+    questionBtn = document.querySelector(".question-button"),
+	closeBtn = document.querySelector(".close-btn"),
+	explainerBox = document.querySelector(".explainer-box"),
+	resetBtn = document.querySelector('.reset-button'),
+	instrumentsContainer = document.querySelector(".instruments");
     let audioToPlay = [];
     let isPlaying = false;
 
@@ -39,39 +39,46 @@
 		});
 
 	});
-
-    playbtn.addEventListener('click', function(e){
-      if(!isPlaying){
-        audioToPlay.forEach(audio => {
-        audio.play();
-      });
-	  isPlaying = true;
-	  controller.classList.add('controller-active');
-	  playbtn.style.backgroundImage = "url(images/stop-btn.svg)";
-    } else {
-      isPlaying = false;
-      audioToPlay.forEach(audio => {
-        audio.pause();
-        audio.currentTime = 0;
-	  });
-	  controller.classList.remove('controller-active');
-	  // 2. Change image to 'play'
-	  playbtn.style.backgroundImage = "url(images/play-btn.svg)";
+    
+	function playController(){
+		if(!isPlaying){
+			audioToPlay.forEach(audio => {
+				audio.play();
+			});
+		  	isPlaying = true;
+		  	controller.classList.add('controller-active');
+		  	playbtn.style.backgroundImage = "url(images/stop-btn.svg)";
+		} else {
+			isPlaying = false;
+		  	audioToPlay.forEach(audio => {
+				audio.pause();
+				audio.currentTime = 0;
+		  	});
+		  	controller.classList.remove('controller-active');
+		  	// 2. Change image to 'play'
+		  	playbtn.style.backgroundImage = "url(images/play-btn.svg)";
+		}
 	}
-	});
 
-	// resetBtn.addEventListener('click', function(){
-	// 	console.log("Reset was pressed");
-	// 	dropZones.forEach(zone => {
-	// 		if(zone.childNodes.length > 0){
-	// 			let elementToAdd = zone.firstElementChild;
-	// 			instruments.appendChild(elementToAdd);
-	// 			// zone.childNodes.remove(zone.firstElementChild);
-	// 		} else {
-	// 			console.log("Node is empty");
-	// 		}
-	// 	})
-	// });
+	resetBtn.addEventListener('click', function(){
+		console.log("Reset was pressed");
+		dropZones.forEach(zone => {
+			if(zone.childNodes.length > 0){
+				instrumentsContainer.appendChild(zone.firstElementChild);
+				isPlaying = false;
+		  		audioToPlay.forEach(audio => {
+					audio.pause();
+					audio.currentTime = 0;
+				});
+				audioToPlay = [];
+		  		controller.classList.remove('controller-active');
+		  		// 2. Change image to 'play'
+		  		playbtn.style.backgroundImage = "url(images/play-btn.svg)";
+			} else {
+				console.log("Node is empty");
+			}
+		})
+	});
 
 	questionBtn.addEventListener('click', function(){
 		explainerBox.classList.add("explainer-box-active");
@@ -82,5 +89,7 @@
 		explainerBox.classList.remove("explainer-box-active");
 		questionBtn.style.display = 'block';
 	});
+
+	playbtn.addEventListener('click', playController);
 
 })();
